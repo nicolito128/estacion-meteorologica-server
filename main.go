9b3b47e89cc7a5f209420a595deaa2281d87c71c
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2026-02-08 17:23:18 -0300

    Revert "up: https"
    This reverts commit 981087c47347183e49d942b4f89efdfc95b9aab8.

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	"log"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/nicolito128/estacion-meteorologica-server/internal/handlers"
 	"github.com/nicolito128/estacion-meteorologica-server/internal/stats"
@@ -21,37 +19,20 @@ func main() {
 		*addr = ":" + *addr
 	}
 
-	tlsConf := tls.Config{
-		MinVersion:               tls.VersionTLS13,
-		PreferServerCipherSuites: true,
-		CurvePreferences: []tls.CurveID{
-			tls.X25519,
-			tls.CurveP256,
-		},
-	}
-
 	// Los datos compartidos entre los manejadores
 	shared := &handlers.SharedContext{
 		Stats: stats.NewStats(),
 	}
 
 	// Creamos el servidor principal que servirá nuestras peticiones HTTP
-	mux := http.NewServeMux()
-	server := &http.Server{
-		Addr:         *addr,
-		Handler:      mux,
-		TLSConfig:    &tlsConf,
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
-		IdleTimeout:  60 * time.Second,
-	}
+	root := http.NewServeMux()
 
 	// Asignamos los manejadores de las rutas
-	handlers.SetupHandlers(mux, shared)
+	handlers.SetupHandlers(root, shared)
 
-	log.Printf("Iniciando servidor en https://127.0.0.1%s/ - CTRL + C para interrumpir", *addr)
+	log.Printf("Iniciando servidor en http://127.0.0.1%s/ - CTRL + C para interrumpir", *addr)
 	// TODO: Deberíamos manejar TLS en algún momento
-	if err := server.ListenAndServeTLS("certificate.crt", "private.key"); err != nil {
+	if err := http.ListenAndServe(*addr, root); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
 }
